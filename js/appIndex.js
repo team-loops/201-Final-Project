3b74bbeac1b9af3e219ece9dfee6eedd1cb29827
DYLAN ULLRICH <dylan@getullrichordietrying.com>
2022-04-26 11:03:52 -0700

    added JS outline

---
+++ js/appIndex.js
@@ -0,0 +1,59 @@
+'use strict'
+
+//--------------------GLOBAL VARIABLES/IMPORTS
+
+
+
+  
+
+
+  
+//--------------------DOM REFERENCES
+
+  
+
+  
+
+  
+
+//--------------------CONSTRUCTORS
+
+  
+
+  
+
+
+
+//--------------------CONSTRUCTOR METHODS
+
+  
+
+  
+
+  
+
+//--------------------FUNCTIONS
+
+  
+
+  
+
+  
+
+//--------------------EVENT LISTENERS
+
+  
+
+  
+
+  
+
+//--------------------EVENT HANDLERS
+
+  
+
+  
+
+  
+
+//--------------------FUNCTION CALLS
